67a00d1716f585c523b7286fef837608e081a48e
Dane Summers <dsummersl@yahoo.com>
2024-09-09 14:04:55 -0400

    feat: adds extmark unit test

--- tests/plenary/sluice/integrations/extmark_spec.lua
+++ tests/plenary/sluice/integrations/extmark_spec.lua
@@ -1,87 +1,48 @@
-local assert = require("luassert")
-local mock = require("luassert.mock")
 local extmark = require("sluice.integrations.extmark_signs")
 
-describe("extmark integration", function()
-  local bufnr = 1
-  local settings = {
-    extmarks = {
-      hl_groups = {"Error", "Warning", "Info"}
-    }
+extmark.vim = {
+  api = {
+    nvim_buf_get_extmarks = function()
+      return {
+        { 1, 4, 0, {
+          line_hl_group = "DiagnosticSignWarn",
+          ns_id = 57,
+          priority = 12,
+          right_gravity = true,
+          sign_hl_group = "DiagnosticSignWarn",
+          sign_text = "A "
+        } },
+        { 12, 13, 0, {
+          invalidate = true,
+          ns_id = 23,
+          priority = 199,
+          right_gravity = true,
+          sign_hl_group = "MiniDiffSignAdd",
+          sign_text = "B "
+        } }
+      }
+    end
   }
+}
 
-  local function create_mock_extmarks()
-    return {
-      {1, 0, 0, {sign_hl_group = "Error", sign_text = "E", priority = 10}},
-      {2, 1, 0, {sign_hl_group = "Warning", sign_text = "W", priority = 5}},
-      {3, 2, 0, {sign_hl_group = "Info", sign_text = "I", priority = 1}},
-      {4, 3, 0, {sign_hl_group = "", sign_text = "", priority = 0}},  -- Should be ignored
-    }
-  end
-
-  before_each(function()
-    extmark.vim = mock(vim, true)
-  end)
-
-  after_each(function()
-    mock.revert(extmark.vim)
-  end)
-
-  describe("update", function()
-    it("should return extmarks as signs", function()
-      extmark.vim.api.nvim_buf_get_extmarks.returns(create_mock_extmarks())
-
-      local result = extmark.update(settings, bufnr)
-
-      assert.is_table(result)
-      assert.equals(3, #result)
-      assert.same({
-        lnum = 1,
-        text = "E",
-        texthl = "Error",
-        priority = 10,
-        plugin = 'extmarks',
-      }, result[1])
-      assert.same({
-        lnum = 2,
-        text = "W",
-        texthl = "Warning",
-        priority = 5,
-        plugin = 'extmarks',
-      }, result[2])
-      assert.same({
-        lnum = 3,
-        text = "I",
-        texthl = "Info",
-        priority = 1,
-        plugin = 'extmarks',
-      }, result[3])
-    end)
-
-    it("should return empty table when no hl_groups", function()
-      local settings_without_hl = {extmarks = {}}
-      local result = extmark.update(settings_without_hl, bufnr)
-      assert.is_table(result)
-      assert.same({}, result)
-    end)
-  end)
-
-  describe("enable", function()
-    it("should not throw an error", function()
-      assert.has_no.errors(function()
-        extmark.enable(settings, bufnr)
-      end)
-    end)
-  end)
-
-  describe("disable", function()
-    it("should clear highlight groups", function()
-      extmark.vim.api.nvim_buf_get_extmarks.returns(create_mock_extmarks())
-      extmark.disable(settings, bufnr)
-      assert.stub(extmark.vim.api.nvim_exec).was.called(3)
-      assert.stub(extmark.vim.api.nvim_exec).was.called_with("hi clear Error", false)
-      assert.stub(extmark.vim.api.nvim_exec).was.called_with("hi clear Warning", false)
-      assert.stub(extmark.vim.api.nvim_exec).was.called_with("hi clear Info", false)
-    end)
+describe("update", function()
+  it("should return extmarks as signs", function()
+    local result = extmark.update({ extmarks = { hl_groups = {} } }, 0)
+    assert.is_table(result)
+    assert.equals(2, #result)
+    assert.same({
+      lnum = 5,
+      text = "A ",
+      texthl = "DiagnosticSignWarn",
+      priority = 12,
+      plugin = 'extmarks',
+    }, result[1])
+    assert.same({
+      lnum = 14,
+      text = "B ",
+      texthl = "MiniDiffSignAdd",
+      priority = 199,
+      plugin = 'extmarks',
+    }, result[2])
   end)
 end)
