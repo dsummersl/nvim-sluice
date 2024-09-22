e35df744f1340ff5f3d88c3d9a8603ae47f0118e
Dane Summers <dsummersl@yahoo.com>
2024-09-22 18:07:37 -0400

    Make only one command rather than three

--- lua/sluice.lua
+++ lua/sluice.lua
@@ -3,12 +3,53 @@ local M = {
 }
 
 M.setup = function(settings)
+  local commands = require('sluice.commands')
   local config = require('sluice.config')
   config.apply_user_settings(settings)
 
-  M.vim.api.nvim_command('command! SluiceEnable  lua require("sluice.commands").enable()')
-  M.vim.api.nvim_command('command! SluiceDisable lua require("sluice.commands").disable()')
-  M.vim.api.nvim_command('command! SluiceToggle lua require("sluice.commands").toggle()')
+  local subcommands = {
+    enable = {
+      impl = function(args, opts)
+        commands.enable()
+      end
+    },
+    disable = {
+      impl = function(args, opts)
+        commands.disable()
+      end
+    },
+    toggle = {
+      impl = function(args, opts)
+        commands.toggle()
+      end
+    },
+  }
+
+  local function sluice_command(opts)
+    local fargs = opts.fargs
+    local subcommand_arg = fargs[1]
+    local args = #fargs > 1 and {table.unpack(fargs, 2)} or {}
+    local subcommand = subcommands[subcommand_arg]
+    if not subcommand then
+      vim.notify("Sluice: unknown command: " .. subcommand, vim.log.levels.ERROR)
+      return
+    end
+
+    subcommand.impl(args, opts)
+  end
+
+  vim.api.nvim_create_user_command("Sluice", sluice_command, {
+    nargs = "+",
+    desc = "Sluice control commands",
+    complete = function (arg_lead, cmdline, _)
+      if cmdline:match("^Sluice%s+%w*$") then
+        local keys = vim.tbl_keys(subcommands)
+        return vim.iter(keys):filter(
+          function(k) return k:match(arg_lead) ~= nil end
+        ):totable()
+      end
+    end
+  })
 
   if config.bool_table_fn(config.settings.enabled) then
     require("sluice.commands").enable()
