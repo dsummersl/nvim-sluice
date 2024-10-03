fcf4a1ff737ae0aea588baf83af64aa2b0bdc96e
Dane Summers <dsummersl@yahoo.com>
2024-10-03 16:25:39 -0400

    feat: Add sluice2.lua

---
+++ lua/sluice2.lua
@@ -0,0 +1,63 @@
+local M = {
+  vim = vim
+}
+
+M.setup = function(settings)
+  local commands = require('sluice2.commands')
+  local config = require('sluice2.config')
+  config.apply_user_settings(settings)
+
+  local subcommands = {
+    enable = {
+      impl = function(args, opts)
+        commands.enable()
+      end
+    },
+    disable = {
+      impl = function(args, opts)
+        commands.disable()
+      end
+    },
+    toggle = {
+      impl = function(args, opts)
+        commands.toggle()
+      end
+    },
+  }
+
+  local function sluice_command(opts)
+    local fargs = opts.fargs
+    local subcommand_arg = fargs[1]
+    local args = #fargs > 1 and {table.unpack(fargs, 2)} or {}
+    local subcommand = subcommands[subcommand_arg]
+    if not subcommand then
+      vim.notify("Sluice: unknown command: " .. subcommand, vim.log.levels.ERROR)
+      return
+    end
+
+    subcommand.impl(args, opts)
+  end
+
+  vim.api.nvim_create_user_command("Sluice", sluice_command, {
+    nargs = "+",
+    desc = "Sluice control commands",
+    complete = function (arg_lead, cmdline, _)
+      if cmdline:match("^Sluice%s+%w*$") then
+        local keys = vim.tbl_keys(subcommands)
+        return vim.iter(keys):filter(
+          function(k) return k:match(arg_lead) ~= nil end
+        ):totable()
+      end
+    end
+  })
+
+  if config.bool_table_fn(config.settings.enabled) then
+    require("sluice2.commands").enable()
+  else
+    require("sluice2.commands").disable()
+  end
+end
+
+M.setup()
+
+return M
